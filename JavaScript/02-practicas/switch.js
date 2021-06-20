6d93d283d667570a243c0bc11d156458ffce4e28
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2021-06-20 18:40:57 -0500

    Create switch.js

---
+++ JavaScript/02-practicas/switch.js
@@ -0,0 +1,20 @@
+var opcion = "Distinto"
+
+switch (opcion) {
+    case 1:
+        console.log("Menu de usuario")
+        break;
+    case 2:
+        console.log("Menu administrativo")
+        break;
+    case 3:
+        console.log("acturacion")
+        break;
+    case "Nuevo Menu":
+        let miNuevoMenu = "Salida"
+        console.log(miNuevoMenu)
+        break;    
+    default:
+        console.log("Break")
+        break;
+}
